5671a318b3774de947de040c4e0eb53513302461
noemi <noemibonilla95@hotmail.com>
2025-05-19 21:32:48 +0200

    fix: update API URLs for production environment

--- client/src/hooks/axios.js
+++ client/src/hooks/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import.meta.env.VITE_API_URL
+
 
 const instance = axios.create({
-    baseURL: VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
