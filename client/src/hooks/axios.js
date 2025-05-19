1a1c671f96f4804efdd885354e7298006edc53b5
noemi <noemibonilla95@hotmail.com>
2025-05-19 22:11:47 +0200

    fix: update axios baseURL and add Vercel configuration

--- client/src/hooks/axios.js
+++ client/src/hooks/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "https://moneyup-n8kx.onrender.com/api/",
+    baseURL: "https://moneyup-n8kx.onrender.com/api",
     withCredentials: true
 })
 
