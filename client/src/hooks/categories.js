1c6009a2517ae39a79bc641bf23a04e5ab0dde5c
noemi <noemibonilla95@hotmail.com>
2025-05-19 21:29:03 +0200

    fix: update API URLs for production environment

--- client/src/hooks/categories.js
+++ client/src/hooks/categories.js
@@ -1,9 +1,9 @@
 import axios from "./axios";
 
 export const getCategoriesRequest = () =>
-  axios.get("categories/get-categories");
+  axios.get("/categories/get-categories");
 export const createCategoriesRequest = (categories) =>
-  axios.post("categories/add-categories", categories);
+  axios.post("/categories/add-categories", categories);
 export const deleteCategoriesRequest = (id) =>
   axios.delete(`/categories/delete/${id}`);
 export const updateCategoriesRequest = (id, categories) =>
--- client/src/hooks/transactions.js
+++ client/src/hooks/transactions.js
@@ -1,7 +1,7 @@
 import axios from "./axios"
 
-export const getTransactionsRequest = ()=> axios.get("expense/all-expenses")
-export const getExpenseRequest = (id) => axios.get(`expense/getExpense/${id}`);
-export const createExpenseRequest = (expense) => axios.post("expense/add-expense", expense);
+export const getTransactionsRequest = ()=> axios.get("/expense/all-expenses")
+export const getExpenseRequest = (id) => axios.get(`/expense/getExpense/${id}`);
+export const createExpenseRequest = (expense) => axios.post("/expense/add-expense", expense);
 export const deleteExpenseRequest = (id) => axios.delete(`/expense/remove/${id}`);
 export const updateExpenseRequest = (id,expense) => axios.put(`/expense/update/${id}`, expense);
