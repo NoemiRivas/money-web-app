5faf9e5738a190c21d78b65dc6dc4480d95a4834
noemi <noemibonilla95@hotmail.com>
2025-05-20 23:02:27 +0200

    fix: add token authorization to axios requests for categories and transactions

--- client/src/hooks/axios.js
+++ client/src/hooks/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useAuth } from "@clerk/clerk-react";
+
 
 
 const instance = axios.create({
@@ -8,15 +8,4 @@ const instance = axios.create({
 });
 console.log("Base URL cargada:", import.meta.env.VITE_API_URL);
 
-instance.interceptors.request.use(
-  async (config) => {
-    const { getToken } = useAuth();
-    const token = await getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 export default instance;
