f9a13a90a24a3fe85cf82ac4bbf63a073e44d304
noemi <noemibonilla95@hotmail.com>
2025-05-20 20:22:17 +0200

    fix: update axios instance to use environment variable for baseURL

--- client/src/hooks/axios.js
+++ client/src/hooks/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://moneyup-n8kx.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 console.log("Base URL cargada:", import.meta.env.VITE_API_URL);
